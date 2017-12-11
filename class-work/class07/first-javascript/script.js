913b7c5a164e22cd6216f4416a7d81c61f646390
nikhiljhunjhnuwala <nikhil@noom.com>
2017-12-11 18:45:45 -0500

    Project proposal text

---
+++ class-work/class07/first-javascript/script.js
@@ -0,0 +1,32 @@
+var product = 10*10;
+var quotient = 100/10;
+var difference = 50-17;
+var sum = 50+17;
+var first = 'Nikhil';
+var last = 'Jhunjhnuwala';
+
+// (first+" "+last)
+// (`my name is ${first} ${last}`)
+
+console.log(product,quotient,difference,sum);
+console.log(`${product}${quotient}${difference}${sum}`);
+
+function test (){
+    var city = 'NYC';
+    console.log(`I live in ${city}`);
+}
+
+test ();
+test ();
+test ();
+
+console.log(document.getElementById('hello'));
+
+var hello = document.getElementById('hello');
+console.log(hello);
+
+
+hello.style.color = 'red';
+
+
+
