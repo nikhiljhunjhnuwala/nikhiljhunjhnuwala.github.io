b4d4474402ae784fe784a3b1b2369c84172270f2
nikhiljhunjhnuwala <nikhil@noom.com>
2017-12-13 20:57:44 -0500

    homework class 09

--- class-work/class08/rgb-color-choice/js/project.js
+++ class-work/class08/rgb-color-choice/js/project.js
@@ -5,7 +5,8 @@ function changeColor(){
     var red = document.getElementById('red').value;
     var green = document.getElementById('green').value;
     var blue = document.getElementById('blue').value;
-    var colorStr = "rgb("+red+", "+green+", "+red+")";
+    // var colorStr = "rgb("+red+", "+green+", "+red+")";
+    var colorStr = `rgb(${red},${green},${blue})`;
     document.getElementById("colorful-text").innerHTML = colorStr;
     var wrapper = document.querySelector('#wrapper');
     wrapper.style.background = colorStr; 
